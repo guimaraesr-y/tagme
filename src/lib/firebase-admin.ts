3567bc5972d39999370a8b4c0fabcb8a836cd376
Ryan Guimarães <guimaraesryan26@gmail.com>
2024-11-02 01:28:43 -0300

    fix: prevent multiple admin apps from being created

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,11 +1,14 @@
-import { applicationDefault, initializeApp } from "firebase-admin/app";
+import { applicationDefault, getApp, getApps, initializeApp } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
 import { getAuth } from "firebase-admin/auth";
 
-export const app = initializeApp({
-    credential: applicationDefault(),
-    databaseURL: 'https://tagme-d0a67.firebaseio.com'
-});
+const adminApp = !getApps().length
+  ? initializeApp({
+      credential: applicationDefault(),
+      databaseURL: "https://tagme-d0a67.firebaseio.com",
+    })
+  : getApp();
 
-export const messaging = getMessaging(app);
-export const auth = getAuth(app);
+export const messaging = getMessaging(adminApp);
+export const auth = getAuth(adminApp);
+export default adminApp;
